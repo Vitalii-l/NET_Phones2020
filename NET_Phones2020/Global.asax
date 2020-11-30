e12725740d91e4b2e21c82549865c412aa326670
pupil <pupil@ivkhk.ee>
2020-11-30 13:33:04 +0200

    Add project files.

---
+++ NET_Phones2020/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NET_Phones2020.MvcApplication" Language="C#" %>
---
+++ NET_Phones2020/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NET_Phones2020.Startup))]
+namespace NET_Phones2020
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
