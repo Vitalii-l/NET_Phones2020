3275b77a822a059ca24953a7de49aa00a0162f4d
pupil <pupil@ivkhk.ee>
2020-11-30 15:04:43 +0200

    Работает список телефонов и детаилз

--- NET_Phones2020/App_Start/RouteConfig.cs
+++ NET_Phones2020/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NET_Phones2020
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Phones", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ NET_Phones2020/Controllers/PhonesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NET_Phones2020.Models;
+
+namespace NET_Phones2020.Controllers
+{
+    public class PhonesController : Controller
+    {
+        private PhonesEntities db = new PhonesEntities();
+
+        // GET: Phones
+        public ActionResult Index()
+        {
+            var phones = db.Phones.Include(p => p.Company).OrderBy(p => p.Name);
+            return View(phones.ToList());
+        }
+
+        // GET: Phones/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Phone phone = db.Phones.Include(p => p.Company).FirstOrDefault(t => t.Id == id);
+            if (phone == null)
+            {
+                return HttpNotFound();
+            }
+            return View(phone);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
